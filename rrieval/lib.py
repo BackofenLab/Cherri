cd7c3480eae35076ede328706ea38c184d487e96
Florian Eggenhofer <egg@tbi.univie.ac.at>
2021-03-23 13:42:17 +0100

    Added function library

---
+++ rrieval/lib.py
@@ -0,0 +1,51 @@
+def calculate_overlap(s1,e1,s2,e2):
+    """
+    Building for each replicat a inter object
+
+        Parameters
+        ----------
+        s1: start of one sequence of the first replicat
+        e1: end of one sequence of the first replicat
+        s2: start of one sequence of the current replicat
+        e2: end of one sequence of the current replicat
+
+
+        Raises
+        ------
+        nothing
+
+        Returns
+        -------
+        compinde_overlap
+            the combined overlap of sequence 1 and sequence 2
+
+        """
+    # print(s1,e1,s2,e2)
+    if s1 <= s2:
+        s_overlap = s2
+        if e1 <= e2:
+            e_overlap = e1
+        elif e2 < e1:
+            e_overlap = e2
+        else:
+            print('error: somthing is not overlaping hier')
+    elif s2 < s1:
+        s_overlap = s1
+        if e1 <= e2:
+            e_overlap = e1
+        elif e2 < e1:
+            e_overlap = e2
+        else:
+            print('error: somthing is not overlaping hier')
+    overlap_len = e_overlap - s_overlap +1
+    seq1_len = e1 - s1 + 1
+    seq2_len = e2 - s2 + 1
+    overlap_seq1 = overlap_len/seq1_len
+    # print(overlap_seq1)
+    overlap_seq2 = overlap_len/seq2_len
+    # print(overlap_seq2)
+    # compinde_overlap = (overlap_seq1 + overlap_seq2)/2
+    # select overlap of shorter sequence:
+    compinde_overlap = max([overlap_seq1, overlap_seq2])
+    # print(compinde_overlap)
+    return compinde_overlap
