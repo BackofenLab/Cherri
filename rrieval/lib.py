480eb952c61f9b203157755bb7488115e816a869
teresa-m <teresa-m@t-online.de>
2021-03-31 14:08:57 +0200

    added pandas read data function to lib

--- rrieval/lib.py
+++ rrieval/lib.py
@@ -1,3 +1,44 @@
+import pandas as pd
+
+def read_chira_data(in_file, header='no'):
+    """
+    Read RRI tabular file and convert to a dataframe including a header
+
+        Parameters
+        ----------
+        in_file : tabular file with output of chira RRI results
+
+        Raises
+        ------
+        nothing
+
+        Returns
+        -------
+        df_interactions
+            dataframe listing all interactions
+
+        """
+    df_temp = pd.read_table(in_file, header=None, sep="\t")
+    # inclued header
+    if header == 'no':
+        header = ['#reads','chrom_1st','start_1st','end_1st', 'strand_1st',
+                'chrom_2end','start_2end','end_2end', 'strand_2end',
+                'ineraction_side_1st', 'ineraction_side_2end',
+                'IntaRNA_prediction', 'energy',
+                'seq_1st_ineraction_side', 'seq_2end_ineraction_side',
+                'start_interaction',
+                'chrom_seq_1st_side', 'start_seq_1st_side',
+                'stop_seq_1st_side','strand_seq_1st_side',
+                'chrom_seq_2end_side', 'start_seq_2end_side',
+                'stop_seq_2end_side','strand_seq_2end_side',
+                'TPM_seq_1st_side', 'TPM_seq_2end_side', 'TPM_summary',
+                'score_seq_1st_side', 'score_seq_2end_side','score_product',
+                'biotype_region_1st', 'biotype_region_2end', 'ID_1st','ID_2end']
+    # len(header)
+    df_interactions = pd.DataFrame(df_temp.values, columns=header)
+    return df_interactions
+
+
 def calculate_overlap(s1,e1,s2,e2):
     """
     Building for each replicat a inter object
