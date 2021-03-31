78a4b2ebc96f81fc1a616b80b27e59ca75ad4861
teresa-m <teresa-m@t-online.de>
2021-03-31 14:06:44 +0200

    added plotting script

---
+++ plot_tRRIs.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import pandas as pd
+#import mathe
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import seaborn as sns
+import argparse
+import os
+from operator import itemgetter
+import rrieval.lib as rl
