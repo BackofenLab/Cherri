fc2728ed8365bb0a1302094280f14c229d0b6ba8
Ubuntu <ubuntu@precisekepler-182c1.openstack.bielefeld>
2023-09-02 14:44:52 +0000

    update temp folder autoskleran

--- rrieval/lib.py
+++ rrieval/lib.py
@@ -1761,6 +1761,7 @@ def perfome_cv(folds, opt_call_cv, out_path_model, midel_name):
                    f'--autosk_debugfile {out_path_model}{midel_name}_'
                    f'fold{fold}_autosklearn.log')
         print(f'\n run optimize call for fold {fold}')
+        print(cv_call)
         out = call_script(cv_call, reprot_stdout=True, asset_err=False)
     f1_df_list = []
     for fold in range(folds):
