2191b49368ee9d5715e310f0a686265ce6fd5480
Ubuntu <ubuntu@precisekepler-182c1.openstack.bielefeld>
2024-01-12 15:18:50 +0000

    changed the plot creation for featrue importance

--- scripts/correlation_matrix.py
+++ scripts/correlation_matrix.py
@@ -2,7 +2,6 @@
 import pandas as pd
 import argparse
 import ubergauss.tools as ut
-from  sklearn.ensemble import RandomForestClassifier as RF
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
@@ -29,23 +28,25 @@ def main():
     X,y, ftname, _ = human_file
 
 
-   # Convert the numpy array to a pandas DataFrame
+    # Convert the numpy array to a pandas DataFrame
     df = pd.DataFrame(X, columns=ftname)
 
-# Calculate the correlation matrix
+    # Calculate the correlation matrix
     corr_matrix = df.corr()
 
-# Cluster the features
+    # Cluster the features
     corr_linkage = hierarchy.ward(corr_matrix)
     dendro = hierarchy.dendrogram(corr_linkage, labels=corr_matrix.columns, no_plot=True)
     # sorted_corr_index = np.array(dendro['ivl'], dtype="int")
 
-# Create a heatmap with the clustered correlation matrix
+    # Create a heatmap with the clustered correlation matrix
     plt.figure(figsize=(10, 8))
     #sns.heatmap(corr_matrix.iloc[sorted_corr_index, sorted_corr_index], annot=True, fmt=".2f", cmap='coolwarm',
     #        xticklabels=corr_matrix.columns[sorted_corr_index], yticklabels=corr_matrix.columns[sorted_corr_index])
     sns.heatmap(corr_matrix)
-# Show the plot
+    # Adjust layout
+    plt.tight_layout()
+    # Show the plot
     plt.title('Clustered Feature Correlation Matrix')
     plt.savefig('feature_correlation_matrix.png', dpi=300) # Saving as PNG with high resolution
 
